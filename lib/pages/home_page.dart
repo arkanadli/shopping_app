904c3c9b529ad1a45642429a5441c74ed304a0ea
arkanadli <uzzomaki@gmail.com>
2023-10-25 09:47:22 +0700

    update bottom nav

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:shopping_app/widgets/product_cart.dart';
 import 'package:shopping_app/widgets/product_list.dart';
@@ -31,26 +32,49 @@ class _HomePageState extends State<HomePage> {
         index: currentPage,
         children: tab,
       ),
-      bottomNavigationBar: BottomNavigationBar(
-          iconSize: 35,
-          selectedFontSize: 0,
-          unselectedFontSize: 0,
-          currentIndex: currentPage,
-          onTap: (value) {
-            setState(() {
-              currentPage = value;
-            });
-          },
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: '',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.shopping_cart_outlined),
-              label: '',
-            )
-          ]),
+      bottomNavigationBar: CurvedNavigationBar(
+        index: currentPage,
+        items: const [
+          Icon(
+            Icons.home,
+            size: 36,
+          ),
+          Icon(
+            Icons.shopping_cart_rounded,
+            size: 36,
+          )
+        ],
+        height: 50,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        buttonBackgroundColor: Theme.of(context).colorScheme.primary,
+        animationDuration: const Duration(milliseconds: 500),
+        animationCurve: Curves.ease,
+        onTap: (value) {
+          setState(() {
+            currentPage = value;
+          });
+        },
+      ),
+      // bottomNavigationBar: BottomNavigationBar(
+      //     iconSize: 35,
+      //     selectedFontSize: 0,
+      //     unselectedFontSize: 0,
+      //     currentIndex: currentPage,
+      //     onTap: (value) {
+      //       setState(() {
+      //         currentPage = value;
+      //       });
+      //     },
+      //     items: const [
+      //       BottomNavigationBarItem(
+      //         icon: Icon(Icons.home),
+      //         label: '',
+      //       ),
+      //       BottomNavigationBarItem(
+      //         icon: Icon(Icons.shopping_cart_outlined),
+      //         label: '',
+      //       )
+      //     ]),
     );
   }
 }
